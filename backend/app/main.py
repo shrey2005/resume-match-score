0e742e77465d418e1b789fd8646dd64d5b1f82a1
shrey2005 <shrey.gajjar1998@gmail.com>
2026-08-19 13:13:51 +0530

    create route for api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api/health")
 def health_check():
     return { 
         "status": "ok"
+    }
+
+ALLOWED_TYPES = {
+    "application/pdf",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+}
+
+@app.post("/api/analyze")
+async def analyze_file(resume: UploadFile = File(...), job_description: str = Form(...)):
+
+    if resume.content_type not in ALLOWED_TYPES:
+        return HTTPException(status_code=400, detail="Invalid file type. Only PDF and DOCX files are allowed.")
+    
+    return {
+        "filename": resume.filename,
+        "job_description": job_description,
+        "message": "File and job description received successfully."
     }
